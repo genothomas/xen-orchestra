a582412cf867806dc8ad1bc625d842c5f7f7aff0
Enishowk <kenshineuh@gmail.com>
2018-12-21 19:08:11 +0100

    add return type

--- @xen-orchestra/fs/src/abstract.js
+++ @xen-orchestra/fs/src/abstract.js
@@ -223,7 +223,7 @@ export default class RemoteHandlerAbstract {
     await this._forget()
   }
 
-  async getInfo() {
+  async getInfo(): Promise<Object> {
     return timeout.call(this._getInfo(), this._timeout)
   }
 
@@ -435,7 +435,7 @@ export default class RemoteHandlerAbstract {
   // called to finalize the remote
   async _forget(): Promise<void> {}
 
-  async _getInfo() {
+  async _getInfo(): Promise<Object> {
     return {}
   }
 
